5e83e70b234667b5e4c2a6c67a6b5771aa130350
Anthony Becker <beckerant131999@gmail.com>
2024-01-25 18:45:34 -0500

    working scrapper

--- webscraper/OMQUARTZSCRAPPER.py
+++ webscraper/OMQUARTZSCRAPPER.py
@@ -1,23 +1,31 @@
-import pandas as pd 
+import pandas as pd
 import requests as r
 from bs4 import BeautifulSoup
 import re
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0 (Edition std-1)"
+}
 
-req1 = r.get('https://omquartz.com/collections/all')
-soup = BeautifulSoup(req1.text, 'html.parser')
-
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0 (Edition std-1)"}
 urls = ['https://omquartz.com/collections/all']
 
-products = soup.find_all('div',class_="card__content")
-prices = soup.find_all('div', class_="price__container")
-for product in products:
-    product_name  = product.find('h3').text
-    print(product_name)
-    if product.find('span', class_='price-item--regular') != None:
-        product_price = product.find('span', class_='price-item--regular').text 
-        print(product_price)
+for url in urls:
+    req = r.get(url, headers=headers)
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    products = soup.find_all('div', class_="card__content")
+
+    for product in products:
+        product_name = product.find('h3').text.strip()
+        
+        product_price_element = product.find('span', class_='price-item--regular')
+        if product_price_element:
+            product_price = product_price_element.text.strip()
+            print("Product Name:", product_name)
+            print("Product Price:", product_price)
+            
+
+        print("\n")
        
         
     
