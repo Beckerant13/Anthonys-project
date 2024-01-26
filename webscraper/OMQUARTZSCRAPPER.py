b8cbe2c03a5c0e244265c1119234c9b9448c0457
Anthony Becker <beckerant131999@gmail.com>
2024-01-26 13:23:14 -0500

    Change

--- webscraper/OMQUARTZSCRAPPER.py
+++ webscraper/OMQUARTZSCRAPPER.py
@@ -2,12 +2,15 @@ import pandas as pd
 import requests as r
 from bs4 import BeautifulSoup
 import re
+import datetime 
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0 (Edition std-1)"
 }
 
-urls = ['https://omquartz.com/collections/all']
+urls = ['https://omquartz.com/collections/all?page=1', 'https://omquartz.com/collections/all?page=2']
+
+all_products = []  
 
 for url in urls:
     req = r.get(url, headers=headers)
@@ -17,14 +20,15 @@ for url in urls:
 
     for product in products:
         product_name = product.find('h3').text.strip()
-        
+
         product_price_element = product.find('span', class_='price-item--regular')
         if product_price_element:
             product_price = product_price_element.text.strip()
-            print("Product Name:", product_name)
-            print("Product Price:", product_price)
-        
-        print("\n")
-       
-        
+            all_products.append({"Product Name": product_name, "Product Price": product_price})
+
+df = pd.DataFrame(all_products)
+df.to_csv('OMQUARTZ.csv', index=False)
+
+print("Data successfully saved to 'OMQUARTZ.csv'")
+
     
