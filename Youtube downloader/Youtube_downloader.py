e1eaf012b271f522c587b39144fc3bb54c56e027
Anthony Becker <beckerant131999@gmail.com>
2024-01-27 15:00:37 -0500

    deleted

--- Youtube downloader/Youtube_downloader.py
+++ Youtube downloader/Youtube_downloader.py
@@ -1,23 +1,4 @@
-from yt_dlp import YoutubeDL
-from sys import argv, exit
-import os
 
-if len(argv) < 2:
-    print("Usage: python Youtube_downloader.py <YouTube_video_link>")
-    exit(1)
-    
-link = argv[1]
 
-output_folder = 'yt vids'
-os.makedirs(output_folder, exist_ok=True)
-
-ydl_opts = {
-    'outtmpl': os.path.join(output_folder, '%(title)s.%(ext)s'),
-}
-
-with YoutubeDL(ydl_opts) as ydl:
-    info_dict = ydl.extract_info(link, download=True)
-
-print("Download completed!")
 
 
